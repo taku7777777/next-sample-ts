5fd29e8cb2703bb7371a5f086792ea48ea220106
takutosato <takuto.sato+recover@bitkey.jp>
2023-09-17 03:10:19 +0900

    sample commit

---
+++ prettier.config.js
@@ -0,0 +1,14 @@
+const config = {
+  printWidth: 120,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  jsxSingleQuote: true,
+  trailingComma: 'es5',
+  bracketSpacing: false,
+  jsxBracketSameLine: false,
+  arrowParens: 'avoid',
+};
+export default config;
